d4fa6ff92e09762fda854b0efdb77f6262f67266
Yuuki Harano <masm+github@masm11.me>
2025-11-02 12:36:12 +0900

    drag

--- puni.c
+++ puni.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
 
 #define WIDTH 640
 #define HEIGHT 480
+
+static int dragging = 0;
+static int start_x = 0, start_y = 0;
+static int now_x = 0, now_y = 0;
 
 static void display(void) {
     //背景を塗りつぶす
@@ -38,11 +43,16 @@ static void display(void) {
     glColor4f(1.0, 0.0, 0.0, 0.0);
     
     // 鼻
+    int dx = 0, dy = 0;
+    if (dragging) {
+	dx = now_x - start_x;
+	dy = -(now_y - start_y);
+    }
     glBegin(GL_QUADS);
-    glVertex3d(-180,   0, 0); //X座標，Y座標，Z座標: Z座標は0
-    glVertex3d(-180, 180, 0);
-    glVertex3d(   0, 180, 0);
-    glVertex3d(   0,   0, 0);
+    glVertex3d(-180 + dx,   0 + dy, 0);
+    glVertex3d(-180 + dx, 180 + dy, 0);
+    glVertex3d(   0,      180,      0);
+    glVertex3d(   0,        0,      0);
     glEnd();
     
     //-----------モデルの描画ここまで-------------------
@@ -57,7 +67,28 @@ static void resize(int windowWidth, int windowHeight) {
 static void timer(int _){
     glutPostRedisplay();
     //この関数自身を100ms後に呼び出す指定
-    glutTimerFunc(100, timer, 0);
+    glutTimerFunc(17, timer, 0);
+}
+
+static void mouse(int button, int state, int x, int y)
+{
+    if (button == 0) {
+	if (state == 0) {
+	    dragging = 1;
+	    start_x = now_x = x;
+	    start_y = now_y = y;
+	} else {
+	    dragging = 0;
+	}
+    }
+}
+
+static void motion(int x, int y)
+{
+    if (dragging) {
+	now_x = x;
+	now_y = y;
+    }
 }
 
 int main(int argc, char *argv[])
@@ -73,12 +104,15 @@ int main(int argc, char *argv[])
     glutDisplayFunc(display);
     glutReshapeFunc(resize); //関数resizeをコールバックに設定
     
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+    
     //黒で塗りつぶす指定をする
     glClearColor(0.0, 0.0, 0.0, 0.0);
     
     //移動させていくためのtimer関数を100ms後に呼び出す指定
     //一度呼んだら指定は消えるので，timer関数内の最後で再呼び出ししなければならない
-    glutTimerFunc(100, timer, 0);
+    glutTimerFunc(17, timer, 0);
     
     //メインループ開始
     glutMainLoop();
